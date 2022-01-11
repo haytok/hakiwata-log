513e9c283af7a4e838d151cc5dba0fbcc1f6e580
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2022-01-12 04:13:02 +0900

    created a client app for verification

---
+++ 20220109/client/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
+def index():
+    return app.send_static_file('html/client.html')
+
+app.run(debug=True, host='0.0.0.0', port=8000)
