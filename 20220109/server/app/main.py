1ea2eb38f1fc1cbaa542164ebb60f88da1274e44
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2022-01-12 05:48:12 +0900

    fix an app for server to disable a CORS method

--- 20220109/server/app/main.py
+++ 20220109/server/app/main.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from flask_cors import CORS 
+
+# from flask_cors import CORS
 
 app = Flask(__name__, static_folder='.', static_url_path='')
-CORS(app)
+# CORS(app)
+
 
 @app.route('/')
 def index():
     return app.send_static_file('html/server.html')
+
 
 app.run(debug=True, host='0.0.0.0', port=8000)
